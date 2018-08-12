c73dbf49ef47110b6111b174422302ae63fd9d91
Udevgn <utkarshdevgan@yahoo.com>
2018-08-12 13:57:07 +0530

    fixed the edge cases .
    LCA function is fixed ....

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -45,22 +45,31 @@ public class BinaryTree{
 	
 	}
 	public boolean LCA_util(Node root,Node A,Node B){
-		if(root == null)
+		if(root == null )
 			return false;
-		return (LCA_util(root.left,A,B) || (root.data == A.data || root.data == B.data)|| LCA_util(root.right,A,B));
+		System.out.println(root.data+" "+A.data+" "+B.data);
+		return (LCA_util(root.left,A,B) || (root.data == A.data || root.data == B.data) || LCA_util(root.right,A,B) );	
 	}
 	public Node LCA(Node root,Node A,Node B){
 		if(root == null)
 			return null;
 		boolean left  = LCA_util(root.left,A,B);
 		boolean right = LCA_util(root.right,A,B);
-		if(left && right )
+		//System.out.println(left+" "+right);
+		if(left && right)
 			return root;
-
-		else if(left== false && right == true)
-			return LCA(root.right,A,B);
-		else if(left == false && right == true)
-			return LCA(root.right,A,B);
+		else if(left== false && right == true){
+			if (root.data == A.data || root.data == B.data)
+				return root;
+			else
+				return LCA(root.right,A,B);
+			}
+		else if(left == true && right == false){
+			if (root.data == A.data || root.data == B.data)
+				return root;
+			else 
+				return LCA(root.left,A,B);
+			}
 		else 
 			return null;
 	}
@@ -70,9 +79,14 @@ public class BinaryTree{
 		bt.root = bt.insertNode(bt.root,2,"",0);
 		bt.root = bt.insertNode(bt.root,3,"R",0);
 		bt.root = bt.insertNode(bt.root,1,"L",0);
+		bt.root = bt.insertNode(bt.root,5,"LL",0);
+		bt.root = bt.insertNode(bt.root,6,"LR",0);
+		bt.root = bt.insertNode(bt.root,7,"RR",0);
 		bt.infixTraversal(bt.root);
-		Node lca = bt.LCA(bt.root,bt.root.left,bt.root.right);
-		System.out.println("least common ancestor:"+lca.data);
+		Node lca = bt.LCA(bt.root,bt.root.left,bt.root.left.left);
+		//System.out.println(bt.root.left.data+" "+bt.root.left.left.data);
+		//System.out.println("least common ancestor:"+lca.data);
+		
 		
 	}
 
